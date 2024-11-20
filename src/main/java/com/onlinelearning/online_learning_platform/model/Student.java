a390a388a9a7f6eed184602365c49db8e8d0ef24
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-11-21 00:16:34 +0200

    add some annotations to save user as student as default

--- src/main/java/com/onlinelearning/online_learning_platform/model/Student.java
+++ src/main/java/com/onlinelearning/online_learning_platform/model/Student.java
@@ -2,16 +2,19 @@ package com.onlinelearning.online_learning_platform.model;
 
 import com.onlinelearning.online_learning_platform.model.enrollment.Enrollment;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Setter
 @Getter
+@SuperBuilder
 @Entity
 @Table(name = "student")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student extends Users{
 
     @Column(name = "birth_date")
