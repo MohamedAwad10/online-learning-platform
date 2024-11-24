0177528c70317586efcdbca43b27f5eb8dca59d3
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-11-24 02:46:39 +0200

    remove unnecessary cascading types

--- src/main/java/com/onlinelearning/online_learning_platform/model/Course.java
+++ src/main/java/com/onlinelearning/online_learning_platform/model/Course.java
@@ -1,17 +1,12 @@
 package com.onlinelearning.online_learning_platform.model;
 
-import com.onlinelearning.online_learning_platform.enums.CourseStatus;
 import com.onlinelearning.online_learning_platform.model.enrollment.Enrollment;
 import com.onlinelearning.online_learning_platform.model.lesson.Lesson;
 import com.onlinelearning.online_learning_platform.model.review.Review;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -21,6 +16,8 @@ import java.util.Set;
 @Setter
 @Getter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "course")
 @Builder // used for mapping
@@ -64,7 +61,6 @@ public class Course {
     private String image;
 
     @NotNull(message = "Tags must not be null")
-    @NotBlank(message = "Tags cannot be blank")
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
             name = "course_tag",
@@ -74,14 +70,12 @@ public class Course {
     private Set<Tag> tags;
 
     @ManyToOne(
-            fetch = FetchType.EAGER,
-            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+            fetch = FetchType.EAGER
     )
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
     @ManyToOne(
-            cascade = {CascadeType.PERSIST, CascadeType.MERGE}, // try not use cascade
             fetch = FetchType.EAGER
     )
     @JoinColumn(name = "category_id")
@@ -100,7 +94,6 @@ public class Course {
 
     @OneToMany(
             mappedBy = "course",
-            cascade = CascadeType.ALL,
             fetch = FetchType.LAZY
     )
     private Set<Review> reviews;
@@ -108,7 +101,7 @@ public class Course {
     @OneToMany(
             mappedBy = "course",
             fetch = FetchType.LAZY,
-            cascade = {CascadeType.PERSIST, CascadeType.REMOVE}
+            cascade = CascadeType.REMOVE
     )
     private Set<Enrollment> enrollments;
 }
