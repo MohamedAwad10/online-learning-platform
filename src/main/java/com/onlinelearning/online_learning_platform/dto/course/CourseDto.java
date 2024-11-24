1a987a2e7ef3ef84753ea969d261fa38c754f0c1
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-11-24 02:46:07 +0200

    fix some fields types

--- src/main/java/com/onlinelearning/online_learning_platform/dto/course/CourseDto.java
+++ src/main/java/com/onlinelearning/online_learning_platform/dto/course/CourseDto.java
@@ -1,8 +1,8 @@
 package com.onlinelearning.online_learning_platform.dto.course;
 
+import com.onlinelearning.online_learning_platform.dto.lesson.LessonDto;
 import com.onlinelearning.online_learning_platform.dto.review.ReviewDto;
-import com.onlinelearning.online_learning_platform.model.Instructor;
-import com.onlinelearning.online_learning_platform.model.lesson.Lesson;
+import com.onlinelearning.online_learning_platform.dto.user.CourseInstructorDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,11 +26,11 @@ public class CourseDto {
 
     private Set<String> tags;
 
-    private Instructor instructor;
+    private CourseInstructorDto instructor;
 
     private String category;
 
-    private Set<Lesson> lessons;
+    private Set<LessonDto> lessons;
 
     private Set<ReviewDto> reviews;
 
