eee8b91e8f03ed27709461227948c18c0fe9fdca
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-11-29 01:17:04 +0200

    add commons file

--- src/main/java/com/onlinelearning/online_learning_platform/commons/Commons.java
+++ src/main/java/com/onlinelearning/online_learning_platform/commons/Commons.java
@@ -1,18 +1,23 @@
 package com.onlinelearning.online_learning_platform.commons;
 
 import com.onlinelearning.online_learning_platform.model.Course;
+import com.onlinelearning.online_learning_platform.model.Student;
 import com.onlinelearning.online_learning_platform.repository.CourseRepository;
+import com.onlinelearning.online_learning_platform.repository.StudentRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-public class CourseCommons {
+public class Commons {
 
     private CourseRepository courseRepository;
 
-    public CourseCommons(CourseRepository courseRepository){
+    private StudentRepository studentRepository;
+
+    public Commons(CourseRepository courseRepository, StudentRepository studentRepository){
         this.courseRepository = courseRepository;
+        this.studentRepository = studentRepository;
     }
 
     public Course checkCourseExist(Integer courseId) throws Exception{
@@ -23,5 +28,14 @@ public class CourseCommons {
         }
 
         return optionalCourse.get();
+    }
+
+    public Student checkStudentExist(Integer studId) throws Exception{
+        Optional<Student> optionalStudent = studentRepository.findById(studId);
+        if(optionalStudent.isEmpty()){
+            throw new Exception("User not found");
+        }
+
+        return optionalStudent.get();
     }
 }
